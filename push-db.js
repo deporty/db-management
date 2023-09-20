2d837f506b5ec1606c32a9bfc9c736b7016be699
Sergio Posada Urrea <sergio.posadaurrea@gmail.com>
2023-09-19 22:44:32 -0500

    Upload all files just at the end of the day. This is a general message.

--- push-db.js
+++ push-db.js
@@ -1,29 +1,25 @@
-const admin = require("firebase-admin");
-const fs = require("fs");
-const path = require("path");
-const firestore = require("@google-cloud/firestore");
-const moment = require("moment");
-var readdirp = require("readdirp");
-const sharp = require("sharp");
+const admin = require('firebase-admin');
+const fs = require('fs');
+const path = require('path');
+const firestore = require('@google-cloud/firestore');
+const moment = require('moment');
+var readdirp = require('readdirp');
+const sharp = require('sharp');
 
 const PARAMS = process.argv.slice(2);
-const DATA_PATH = "./data";
-const FILES_PATH = "./files";
+const DATA_PATH = './data';
+const FILES_PATH = './files';
 
-const CONFIGURATION = require("./configuration.json");
-if (
-  PARAMS.length < 2 ||
-  CONFIGURATION["environments-list"].indexOf(PARAMS[0]) == -1
-) {
-  console.log(
-    "You must pass ENV and date to recovery. You cand pass a specific collection to recovery"
-  );
+const CONFIGURATION = require('./configuration.json');
+if (PARAMS.length < 2 || CONFIGURATION['environments-list'].indexOf(PARAMS[0]) == -1) {
+  console.log('You must pass ENV and date to recovery. You cand pass a specific collection to recovery');
   process.exit();
 }
 
 const ENV = PARAMS[0];
 const FILE = PARAMS[1];
 const COLLECTION = PARAMS[2];
+const DOC = PARAMS[3];
 
 const serviceAccount = require(`./deporty-${ENV}.json`);
 
@@ -33,7 +29,7 @@ admin.initializeApp({
 });
 
 function generateDocumentReference(data) {
-  const fragments = data.split("/");
+  const fragments = data.split('/');
   const _ = (db, flag, fragments, i) => {
     if (i < fragments.length) {
       if (flag) {
@@ -55,20 +51,20 @@ function generateGeopoint(data) {
 }
 function uparseItem(data) {
   let response = null;
-  if (data.type == "Array") {
+  if (data.type == 'Array') {
     const tempArray = [];
     for (const item of data.value) {
       const temo = uparseItem(item);
       tempArray.push(temo);
     }
     response = tempArray;
-  } else if (data.type == "date") {
+  } else if (data.type == 'date') {
     response = moment(data.value).toDate();
-  } else if (data.type == "reference") {
+  } else if (data.type == 'reference') {
     response = generateDocumentReference(data.value);
-  } else if (data.type == "GeoPoint") {
+  } else if (data.type == 'GeoPoint') {
     response = generateGeopoint(data.value);
-  } else if (data.type === "object") {
+  } else if (data.type === 'object') {
     const temp = {};
     for (const key in data.value) {
       const element = data.value[key];
@@ -83,24 +79,24 @@ function uparseItem(data) {
 }
 
 async function y(storage) {
-  const bucketName = CONFIGURATION["environments"][ENV].bucket;
+  const bucketName = CONFIGURATION['environments'][ENV].bucket;
   const filesFolderName = `${FILES_PATH}/${ENV}/${FILE}`;
   console.log(filesFolderName);
   var settings = {
-    types: "files",
-    fileFilter: ["*.*"],
+    types: 'files',
+    fileFilter: ['*.*'],
   };
   const a = readdirp(filesFolderName, settings);
   var allFilePaths = [];
-  a.on("data", function (entry) {
+  a.on('data', function (entry) {
     allFilePaths.push(entry);
-  }).on("end", async () => {
+  }).on('end', async () => {
     const bucket = storage.bucket(bucketName);
 
     for (const fi of allFilePaths) {
       console.log(fi.path);
       await bucket.upload(fi.fullPath, {
-        destination: fi.path.replace(/\\/g, "/"),
+        destination: fi.path.replace(/\\/g, '/'),
       });
     }
   });
@@ -119,21 +115,13 @@ async function x(db, relativePatch, isCollection = true) {
       if (data) {
         ref.update(parseditem);
       } else {
-        if (
-          relativePatch ===
-          "./data/dev/generated copy/tournaments/7KqZutvODrgNeypckDrO/fixture-stages/qriXXV6Yh7qJLVtFtElk/groups/d7c56b5ced674848ad41/teams"
-        ) {
-          console.log(doc);
-          console.log(content);
-          console.log(parseditem);
-        }
         ref.set(parseditem);
       }
 
       await x(ref, docPath, !isCollection);
     }
   } else {
-    const filteredFolders = files.filter((x) => x != "data.json");
+    const filteredFolders = files.filter((x) => x != 'data.json');
     for (const collection of filteredFolders) {
       const ref = db.collection(collection);
       const newPath = `${relativePatch}/${collection}`;
@@ -148,7 +136,19 @@ async function main() {
 
   const pathFolder = `${DATA_PATH}/${ENV}/${FILE}`;
   // await y(storage);
-  await x(db, pathFolder);
+
+  console.log(COLLECTION, `.${DOC}.`);
+  if (COLLECTION != '') {
+    if (DOC) {
+      const t = db.collection(COLLECTION).doc(DOC);
+      await x(t, `${pathFolder}/${COLLECTION}/${DOC}`);
+    } else {
+      const t = db.collection(COLLECTION);
+      await x(t, `${pathFolder}/${COLLECTION}`, false);
+    }
+  } else {
+    await x(db, pathFolder);
+  }
 }
 
 main();
